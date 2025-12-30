01620a8927a2e846aa94e67cabb5fec5c122497a
ShyamSundar-SF4699 <shyamsundar.d@syncfusion.com>
2025-12-30 15:39:26 +0530

    Included the TabView gradient background sample

---
+++ TabViewGradientBackground/TabViewGradientBackground/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TabViewGradientBackground
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TabViewGradientBackground/TabViewGradientBackground/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace TabViewGradientBackground
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        public ObservableCollection<Model> Tabs { get; } = new()
+        {
+            new Model { Title = "Home",   ContentText = "Home content" },
+            new Model { Title = "Tasks",  ContentText = "Tasks content" },
+            new Model { Title = "Reports",ContentText = "Reports content" }
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private async Task OnRefreshClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Refreshed", "Content was refreshed.", "OK");
+        }
+
+        private async Task OnMoreClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("More", "More actions coming soon.", "OK");
+        }
+
+
+        private void OnCloseClicked(object sender, System.EventArgs e)
+        {
+            if (sender is Button btn && btn.CommandParameter is Model tab && Tabs.Contains(tab))
+                Tabs.Remove(tab);
+        }
+    }
+
+    public class Model
+    {
+        public string Title { get; set; }
+        public string ContentText { get; set; }
+    }
+}
